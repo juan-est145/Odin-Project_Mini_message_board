d6df710a1e2c3505c1325444414ddaa561093e7b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-10 12:47:46 +0200

    Root has now a router too

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const authorRouter = require("./routes/aboutRouter");
+const rootRouter = require("./routes/rootRouter");
+const path = require("node:path");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", rootRouter);
 app.use("/about", authorRouter);
-app.get("/", (req, res) => res.send("Hello world"));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
---
+++ src/controllers/rootController.js
@@ -0,0 +1,5 @@
+function getRoot(req, res) {
+	res.send("Hello world");
+}
+
+module.exports = { getRoot };
