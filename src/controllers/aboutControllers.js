862b4b6433c6c616de56478c45d2421e0b3b1afc
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-10 12:06:06 +0200

    Added get controller

---
+++ src/controllers/aboutControllers.js
@@ -0,0 +1,5 @@
+function getAboutPage(req, res) {
+	res.send("This is my about page");
+}
+
+module.exports = { getAboutPage };
--- src/routes/aboutRouter.js
+++ src/routes/aboutRouter.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
+const aboutControllers = require("../controllers/aboutControllers");
 
 const aboutRouter = Router();
 
-aboutRouter.get("/", (req, res) => {
-	res.send("This is my about page");
-});
+aboutRouter.get("/", aboutControllers.getAboutPage);
 
 module.exports = aboutRouter;
