573e863201710acae3c8664b78c157689762425d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-14 16:33:45 +0200

    Basic project set up is working

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const authorRouter = require("./routes/aboutRouter");
-const rootRouter = require("./routes/rootRouter");
+const indexRouter = require("./routes/indexRouter");
+const newMsgRouter = require("./routes/newMsgRouter");
 const path = require("node:path");
 
+app.use("/", indexRouter);
+app.use("/new", newMsgRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
---
+++ routes/newMsgRouter.js
@@ -0,0 +1,9 @@
+const {Router} = require("express");
+
+const newMsgRouter = Router();
+
+newMsgRouter.get("/",(req, res) => {
+	res.send("Estás en new message");
+});
+
+module.exports = newMsgRouter;
