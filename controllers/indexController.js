c963a460e5169d847c8071270fc6620c33b2d11b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-15 17:59:16 +0200

    We have invalid params for message url handled

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,7 +5,11 @@ function indexRender(req, res) {
 }
 
 function renderMsg(req, res) {
-	const index = req.params.id;
+	const index = parseInt(req.params.id);
+	if (!Number.isInteger(index) || index < 0 || index >= messages.length) {
+		res.status(404).send("404: Message not found");
+		return ;
+	}
 	res.render("viewMsg", {
 		index: index,
 		user: messages[index].user,
