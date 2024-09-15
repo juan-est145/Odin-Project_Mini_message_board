bb958c8fc842713f072912589fb84122c901c809
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-15 13:01:42 +0200

    We now serve static files too

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const indexRouter = require("./routes/indexRouter");
 const newMsgRouter = require("./routes/newMsgRouter");
 const path = require("node:path");
 
+const assetPath = path.join(__dirname, "./public");
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newMsgRouter);
